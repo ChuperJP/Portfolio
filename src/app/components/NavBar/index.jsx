a69f0dca0c75cbf3a345941f4fec024d26ae9440
chuper <chuper@gmx.fr>
2023-06-07 00:03:48 +0200

    #

--- src/app/components/NavBar/index.jsx
+++ src/app/components/NavBar/index.jsx
@@ -24,10 +24,12 @@ export default function NavBar() {
     if (isOpen)
       return (
         <nav className=" flex flex-col justify-around items-center pb-[20px] w-full h-[200px] laptop:hidden">
-          <Link href="/#about" onClick={toggleMenu}>
-            <div className="cursor-pointer text-textM hover:border-b-2 ">
-              A propos
-            </div>
+          <Link
+            href="/#about"
+            onClick={toggleMenu}
+            className="cursor-pointer text-textM hover:border-b-2 "
+          >
+            A propos
           </Link>
           <Link
             href="/#competences"
