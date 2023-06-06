3a559f073156f323b762fd7326e6d8e817459e98
chuper <chuper@gmx.fr>
2023-06-06 21:41:54 +0200

    add bouton component

---
+++ src/app/components/Bouton/index.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+export default function Bouton({ children, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-fourth-color font-montserrat text-textM font-semibold text-white px-[24px] py-[8px] rounded-[20px] cursor-pointer hover:scale-110"
+    >
+      {children}
+    </button>
+  );
+}
