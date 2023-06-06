2ca977e88831712a03a2b8af8e0587543f6bea20
chuper <chuper@gmx.fr>
2023-06-06 21:44:19 +0200

    add link contact component

---
+++ src/app/components/LinkContact/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function LinkContact() {
+  return (
+    <a
+      href="/#contact"
+      className="bg-fourth-color font-montserrat text-textM font-semibold text-white px-[24px] py-[8px] rounded-[20px] cursor-pointer hover:scale-110"
+    >
+      Contact
+    </a>
+  );
+}
