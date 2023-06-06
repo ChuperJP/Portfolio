330f6da2307a67c290ae1bf7b5468af596ae7080
chuper <chuper@gmx.fr>
2023-06-06 20:54:38 +0200

    modification fichier de bases

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        mulish: ["Mulish", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        "first-color": "#082570",
+        "second-color": "#2A3E70",
+        "third-color": "#FAFAFA",
+        "fourth-color": "#5A85F2",
+        "html-color": "#F97316",
+        "react-color": "#38BDF8",
+        "css-color": "#2563EB",
+        "sass-color": "#EC4899",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        headerImg1: "url('/Images/bgHeader.webp')",
+        headerImg2: "url('/Images/bgHeader2.webp')",
+        headerImg3: "url('/Images/bgHeader3.webp')",
+        headerImg4: "url('/Images/bgHeader4.webp')",
+        headerImg5: "url('/Images/bgHeader5.webp')",
+      },
+      fontSize: {
+        H1: "48px",
+        title: "40px",
+        titleS: "32px",
+        textL: "24px",
+        textM: "16px",
+        textS: "12px",
+        textXS: "8px",
+      },
+      screens: {
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
       },
     },
   },
   plugins: [],
-}
+};
