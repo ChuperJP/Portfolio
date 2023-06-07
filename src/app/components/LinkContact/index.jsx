b77d02502be50ac256cb14518ce1d41ea8381b6d
chuper <chuper@gmx.fr>
2023-06-07 12:54:32 +0200

    #

--- src/app/components/LinkContact/index.jsx
+++ src/app/components/LinkContact/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function LinkContact() {
   return (
     <a
-      href="/#contact"
+      href="/portfolio/#contact"
       className="bg-fourth-color font-montserrat text-textM font-semibold text-white px-[24px] py-[8px] rounded-[20px] cursor-pointer hover:scale-110"
     >
       Contact
