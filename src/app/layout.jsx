b8411c50592f42ffaaffa892203504749e0a98bc
chuper <chuper@gmx.fr>
2023-06-07 20:31:48 +0200

    #

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Collet jean-patrick",
@@ -12,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className={inter.className}>
+      <body>
         <NavBar />
         {children}
         <Footer />
