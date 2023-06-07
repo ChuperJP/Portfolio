2caaeec76b4db506c4a67cc650ed6238f3fea137
chuper <chuper@gmx.fr>
2023-06-07 20:31:32 +0200

    add recaptcha v2, reset input after sending message, display success or error message after sending

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
     EMAIL_JS_SERVICE: "service_chuper_dev_gmail",
     EMAIL_JS_TEMPLATE: "contact_form",
     EMAIL_JS_USER: "2s0f3x5iiKwmh-MKW",
+    RECAPTCHA_SITE_KEY: "6LdNZngmAAAAAD8ZtTVY2O5aSbcj0VbKUp73T0p3",
   },
 };
 
--- src/app/components/ContactForm/index.jsx
+++ src/app/components/ContactForm/index.jsx
@@ -1,36 +1,67 @@
 "use client";
-import React, { useRef } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import Bouton from "../Bouton";
 
 export default function ContactForm() {
   const form = useRef();
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const successMessage = () => {
+    if (success) {
+      return (
+        <p className="font-montserrat text-textM text-teal-400 text-center">
+          Votre message a bien été envoyé !
+        </p>
+      );
+    }
+  };
+  const echecMessage = () => {
+    if (error) {
+      return (
+        <p className="font-montserrat text-textM text-red-600">
+          Une erreur s'est produite lors de l'envoie de votre message
+        </p>
+      );
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    setSuccess(false);
+    setError(false);
 
-    // emailjs
-    //   .sendForm(
-    //     process.env.EMAIL_JS_SERVICE,
-    //     process.env.EMAIL_JS_TEMPLATE,
-    //     form.current,
-    //     process.env.EMAIL_JS_USER
-    //   )
-    //   .then(
-    //     (result) => {
-    //       console.log(result.text);
-    //       console.log(result);
-    //     },
-    //     (error) => {
-    //       console.log(error.text);
-    //     }
-    //   );
+    emailjs
+      .sendForm(
+        process.env.EMAIL_JS_SERVICE,
+        process.env.EMAIL_JS_TEMPLATE,
+        form.current,
+        process.env.EMAIL_JS_USER
+      )
+      .then(
+        (result) => {
+          setSuccess(true);
+          setName("");
+          setEmail("");
+          setMessage("");
+        },
+        (error) => {
+          setError(true);
+        }
+      );
   };
 
   return (
     <form
       ref={form}
       onSubmit={handleSubmit}
-      className="bg-first-color rounded-[20px] pt-[30px] pb-[40px] px-[80px] mt-[80px] w-full max-w-[400px]"
+      className="relative bg-first-color rounded-[20px] pt-[30px] pb-[40px] px-[80px] mt-[80px] w-full max-w-[400px] font-montserrat font-semibold"
     >
       <div className="flex flex-col items-center">
         <div className="input-wrapper">
@@ -41,6 +72,8 @@ export default function ContactForm() {
             type="text"
             id="name"
             name="user_name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             required
             className="rounded-[10px] text-first-color"
           />
@@ -53,6 +86,8 @@ export default function ContactForm() {
             type="email"
             id="email"
             name="user_email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             required
             className="rounded-[10px] text-first-color"
           />
@@ -61,14 +96,23 @@ export default function ContactForm() {
           <label htmlFor="message" className="text-white text-textM">
             Message
           </label>
+          {successMessage()}
+          {echecMessage()}
           <textarea
             name="message"
             id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
             rows="7"
             required
             className="rounded-[10px] text-first-color"
           ></textarea>
         </div>
+        <ReCAPTCHA
+          sitekey={process.env.RECAPTCHA_SITE_KEY}
+          theme="dark"
+          className="mb-[30px]"
+        />
         <Bouton children={"Envoyer"} type={"submit"} />
       </div>
     </form>
