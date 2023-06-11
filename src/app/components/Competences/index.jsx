165f9e6684768776516aa0f9531da1a01f4943d0
chuper <chuper@gmx.fr>
2023-06-11 14:04:35 +0200

    ajout animation pour compétences

--- src/app/components/Competences/index.jsx
+++ src/app/components/Competences/index.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import Image from "next/image";
+import { motion, useInView } from "framer-motion";
 import html_icon from "../../assets/html5-icon.png";
 import css_icon from "../../assets/css3-icon.png";
 import js_icon from "../../assets/js-icon.png";
@@ -9,20 +11,42 @@ import sass_icon from "../../assets/sass-icon.png";
 import tailwind_icon from "../../assets/tailwind-icon.png";
 
 export default function Competences() {
+  const ref = useRef(null);
+  // once a true permet de faire l'animation qu'une seule fois, sans ca a chaque appartition sur l'écran l'animation se lance
+  const isInView = useInView(ref, { once: true });
+  const displayCompetences = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: { opacity: 1 },
+  };
+
   return (
     <section id="competences" className=" bg-third-color">
       <div className="flex flex-col items-center mx-[10%] pt-[70px] pb-[100px] ">
         <h2 className="font-montserrat font-semibold text-titleS mb-[80px] tablet:text-title">
           Competences
         </h2>
-        <div className="flex flex-col items-center w-full">
+        <div ref={ref} className="flex flex-col items-center w-full">
           <div className="flex flex-col items-center w-full laptop:flex-row ">
-            <div className="flex flex-row justify-around items-around w-full laptop:w-1/2">
+            <motion.div
+              className="flex flex-row justify-around items-around w-full laptop:w-1/2"
+              initial="hidden"
+              animate={isInView ? "visible" : "hidden"}
+              transition={{ delay: 0.3, duration: 0.5 }}
+              variants={displayCompetences}
+            >
               <Image src={html_icon} alt="html icon" width={70} height={100} />
               <Image src={css_icon} alt="css icon" width={70} height={100} />
               <Image src={js_icon} alt="js icon" width={70} height={100} />
-            </div>
-            <div className="flex flex-row justify-around  mt-[80px] w-full laptop:mt-0 laptop:w-1/2">
+            </motion.div>
+            <motion.div
+              className="flex flex-row justify-around  mt-[80px] w-full laptop:mt-0 laptop:w-1/2"
+              initial="hidden"
+              animate={isInView ? "visible" : "hidden"}
+              transition={{ delay: 0.6, duration: 0.5 }}
+              variants={displayCompetences}
+            >
               <Image
                 className="h-[80px] w-[90px]"
                 src={react_icon}
@@ -37,9 +61,15 @@ export default function Competences() {
                 width={165}
                 height={100}
               />
-            </div>
+            </motion.div>
           </div>
-          <div className="flex flex-row justify-around w-full mt-[80px] laptop:w-1/2">
+          <motion.div
+            className="flex flex-row justify-around w-full mt-[80px] laptop:w-1/2"
+            initial="hidden"
+            animate={isInView ? "visible" : "hidden"}
+            transition={{ delay: 0.9, duration: 0.5 }}
+            variants={displayCompetences}
+          >
             <Image
               className="h-[80px] w-[113px]"
               src={sass_icon}
@@ -54,7 +84,7 @@ export default function Competences() {
               width={166}
               height={100}
             />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
