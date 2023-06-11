0d6bca1e260dc5f77826078cf0aac5fa29d81913
chuper <chuper@gmx.fr>
2023-06-11 14:11:06 +0200

    changement adress mail header

--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   const infos = [
     { id: "1", type: "Nom", text: "Jean-Patrick Collet" },
     { id: "2", type: "Localisation", text: "Île-de-France, France" },
-    { id: "3", type: "E-mail", text: "chuper.dev@gmx.fr" },
+    { id: "3", type: "E-mail", text: "chuper.dev@mail.com" },
   ];
 
   return (
