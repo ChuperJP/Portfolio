76fec823a5159f5cb5d023a59711cfdcc61355bc
chuper <chuper@gmx.fr>
2023-06-13 16:21:41 +0200

    remise mediaquery

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,16 @@ module.exports = {
         textS: "12px",
         textXS: "8px",
       },
+      screens: {
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
