a997e9241be94fdbe70f43de446ee4302db55740
chuper <chuper@gmx.fr>
2023-06-13 17:54:15 +0200

    test preflight

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,10 +40,10 @@ module.exports = {
         desktop: "1280px",
         // => @media (min-width: 1280px) { ... }
       },
-      corePlugins: {
-        preflight: false,
-      },
     },
+  },
+  corePlugins: {
+    preflight: false,
   },
   plugins: [],
 };
