30ba3ee0cc4415a044ea52249705ab827bc36fd0
chuper <chuper@gmx.fr>
2023-06-13 20:24:56 +0200

    #

--- src/app/components/About/index.jsx
+++ src/app/components/About/index.jsx
@@ -10,7 +10,7 @@ export default function About() {
         <h2 className="font-montserrat font-semibold text-titleS text-white mb-[80px] laptop:text-title">
           A propos
         </h2>
-        <div className="font-mulish">
+        <div className="font-mulish text-center text-textM text-white w-full tablet:text-textL laptop:w-2/3 laptop:text-start">
           <p>
             Salut ! je m'appelle Jean-Patrick, je m'interesse au domaine de
             l'informatique depuis mon adolescence, j'ai cependant choisis une
