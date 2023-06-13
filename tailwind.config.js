3446665a949b47311e6a00080340bf58de11d1f0
chuper <chuper@gmx.fr>
2023-06-13 16:18:12 +0200

    remise color retrait mediaquery

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ module.exports = {
         mulish: ["Mulish", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
       },
+      colors: {
+        "first-color": "#082570",
+        "second-color": "#2A3E70",
+        "third-color": "#FAFAFA",
+        "fourth-color": "#6590F6",
+        "html-color": "#F97316",
+        "react-color": "#38BDF8",
+        "css-color": "#2563EB",
+        "sass-color": "#EC4899",
+      },
       fontSize: {
         H1: "48px",
         title: "40px",
@@ -19,16 +29,6 @@ module.exports = {
         textM: "16px",
         textS: "12px",
         textXS: "8px",
-      },
-      screens: {
-        tablet: "640px",
-        // => @media (min-width: 640px) { ... }
-
-        laptop: "1024px",
-        // => @media (min-width: 1024px) { ... }
-
-        desktop: "1280px",
-        // => @media (min-width: 1280px) { ... }
       },
     },
   },
