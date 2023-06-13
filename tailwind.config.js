c62d32498f08d55e59297b68ceaf7ec785cbbc35
chuper <chuper@gmx.fr>
2023-06-13 16:10:35 +0200

    remise font size retrait color

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,14 @@ module.exports = {
         mulish: ["Mulish", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
       },
-      colors: {
-        "first-color": "#082570",
-        "second-color": "#2A3E70",
-        "third-color": "#FAFAFA",
-        "fourth-color": "#6590F6",
-        "html-color": "#F97316",
-        "react-color": "#38BDF8",
-        "css-color": "#2563EB",
-        "sass-color": "#EC4899",
+      fontSize: {
+        H1: "48px",
+        title: "40px",
+        titleS: "32px",
+        textL: "24px",
+        textM: "16px",
+        textS: "12px",
+        textXS: "8px",
       },
       screens: {
         tablet: "640px",
